2ac87f1cb80713cb1dfb49d01dc34a40e926bf23
Bogdan Steblewski <58295548+bogdan-steblewski@users.noreply.github.com>
2020-06-12 15:25:30 +0000

    Update Macierz.cpp

--- src/Macierz.cpp
+++ src/Macierz.cpp
@@ -1,11 +1,5 @@
 #include "Macierz.hh"
 
-/*
- *  Tutaj nalezy zdefiniowac odpowiednie metody
- *  klasy Macierz, ktore zawieraja wiecej kodu
- *  niz dwie linijki.
- *  Mniejsze metody mozna definiwac w ciele klasy.
- */
 std::istream &operator>>(std::istream &Strm, Macierz &Mac)
 {
     for (int i = 0; i < ROZMIAR; i++)
